23dd072ca38f0fd95e10d8a40883bc4ac8ea982c
Anders Langlands <anderslanglands@gmail.com>
2021-04-23 13:02:36 +1200

    fix version

--- bind.sh
+++ bind.sh
@@ -6,4 +6,4 @@ rm -rf build
 
 astgen bind -u -v 1 -o build/ast -- -I${OCIO_ROOT}/include
 
-asttoc build/ast -o build -p ocio -L $OCIO_ROOT/lib -l OpenColorIO -major 1 -minor 1 -patch 1
+asttoc build/ast -o build -p ocio -L $OCIO_ROOT/lib -l OpenColorIO -major 2 -minor 0 -patch 0
